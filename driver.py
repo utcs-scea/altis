d759213f1af5897f6c6047fafd6aa9ada2994864
sarah wang <sarahgw@cs.utexas.edu>
2018-02-01 15:48:46 -0600

    updated driver

--- driver.py
+++ driver.py
@@ -1,44 +1,53 @@
 import os
+import subprocess
 import sys
+from optparse import OptionParser
 
-level0 = ['BusSpeedDownload', 'BusSpeedReadback', 'DeviceMemory', 'MaxFlops']
+suite = {'BusSpeedDownload':0, 'BusSpeedReadback':0, 'DeviceMemory':0, 'MaxFlops':0}
 
 # TODO: Get problem size based on device capabilities
 def get_problem_size():
     return 1
 
-# Write header
-# f: file to write to
-def write_header(f):
-    f.write('#!/bin/bash\n')
-
-# Write command to execute the benchmark
-# f:     file to write to
-# b_dir: directory of benchmark
-# b:     name of benchmark
-# sz:    problem size
-def write_cmd(f, b_dir, b, sz):
-    exec_name = os.path.join(b_dir, b)
-    result_name = os.path.join(result_dir, b)
-    f.write('.%s --outfile %s -s %d\n' % (exec_name, result_name, sz))
+def run_benchmark(options, b):
+    print('Running: %s' % b)
+    # Path of results file
+    result_path = os.path.join(options.prefix, 'results/%s' % (b))
+    # Path of executble
+    exec_path = os.path.join(options.prefix, 'src/cuda/level%d/%s/%s' % (suite[b], b, b))
+    # Execute benchmark with options
+    p = subprocess.Popen([exec_path, '-s', str(options.size), '-f', result_path, '-d', str(options.device)])
+    (stdoutdata, stderrdata) = p.communicate()
+    print(str(type(stdoutdata)))
+    #print('***OUT: ' + stdoutdata)
+    print(str(type(stdoutdata)))
+    #print('***ERR: ' + stderrdata)
 
 if __name__ == '__main__':
-    # Get root directory
-    if len(sys.argv) == 1:
-        prefix = os.getcwd()
-    else:
-        prefix = sys.argv[1]
+    # Options
+    parser = OptionParser()
+    parser.add_option('-p', '--prefix', default=os.getcwd(), help='location of Mirovia root')
+    parser.add_option('-d', '--device', default=0, help='device to run the benchmarks on')
+    parser.add_option('-s', '--size', default=-1, help='problem size')
+    parser.add_option('-b', '--benchmark', default='all', help='comma-separated list of benchmarks to run, or \'all\' to run all')
     
-    # Path of results folder
-    result_dir = os.path.join(prefix, 'results')
-    
+    # Parse options
+    (options, args) = parser.parse_args()
+
     # Problem size
-    sz = get_problem_size()
+    if int(options.size) <= 0:
+        options.size = get_problem_size()
     
-    level_dir = os.path.join(prefix, 'src/cuda/level0')
-    for b in level0:
-        b_dir = os.path.join(level_dir, b)
-        with open(os.path.join(b_dir, 'run'), 'w') as f:
-            write_header(f)
-            write_cmd(f, b_dir, b, sz)
+    # Benchmarks
+    if options.benchmark == 'all':
+        benchmarks = list(suite.keys())
+    else:
+        benchmarks = options.benchmark.split(',')
+
+    # Run benchmarks
+    print('Prefix: %s' % (options.prefix))
+    print('Device: %s' % (options.device))
+    print('Problem size: %s' % (options.size))
+    for b in benchmarks:
+        run_benchmark(options, b)
 
