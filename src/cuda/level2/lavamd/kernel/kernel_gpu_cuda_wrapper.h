fe29ad80d4ebec64be9c7245bdbea69645dac28b
Christopher Denny <cdenny@zemaitis.csres.utexas.edu>
2018-02-22 20:00:52 -0600

    added passes and resultDB logging to lavamd
    Former-commit-id: 2c5429399cd7c4bf059ace7978237679bf2b0cfd
    Former-commit-id: 549373cf9f81de337cf63460732a7b323fce8265
    Former-commit-id: c3c19ae8332bb86d7f5d617f0338eaa0645a0cfe

--- src/cuda/level2/lavamd/kernel/kernel_gpu_cuda_wrapper.h
+++ src/cuda/level2/lavamd/kernel/kernel_gpu_cuda_wrapper.h
@@ -11,7 +11,8 @@ void kernel_gpu_cuda_wrapper(	par_str parms_cpu,
 								box_str* box_cpu,
 								FOUR_VECTOR* rv_cpu,
 								fp* qv_cpu,
-								FOUR_VECTOR* fv_cpu);
+								FOUR_VECTOR* fv_cpu,
+                                ResultDatabase &resultDB);
 
 #ifdef __cplusplus
 }
