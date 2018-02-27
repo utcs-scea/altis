d876a43b4dcaffe849f87d81aee9e349d3aafffb
sarah wang <sarahgw@cs.utexas.edu>
2018-02-26 20:49:56 -0600

    done w bfs, doing datagen now
    Former-commit-id: 8183e4f44d97ea3928936abbbd3829064db76f50
    Former-commit-id: f3661f888803752c75080a376d6404192ac79849
    Former-commit-id: 031c5b6984ed748837008529a958e2386e70696e

---
+++ src/cuda/level1/bfs/compare.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 16M
+    do
+        ./rbfs inputs/graph$i.txt > res_rodinia
+        ./bfs -i inputs/graph$i.txt -o log > res_mirovia
+        (diff res_rodinia res_mirovia) >> test
+        (diff result.txt r.txt) >> test_res
+    done
