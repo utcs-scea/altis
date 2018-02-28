9437cd368c19cfab11092d2d22d4040e28a89beb
sarah wang <sarahgw@cs.utexas.edu>
2018-02-27 21:09:03 -0600

    finished bfs, takes datagen from python
    Former-commit-id: 60ae26f7a98e7e297c7bd7a4e1a76956267849f9
    Former-commit-id: 7a5df5bf77e0ae85e93de9d70007713362f3fed3
    Former-commit-id: 2cb4433c84472a2c5990f410fae759fd0bb912f3

--- src/cuda/level1/bfs/compare.sh
+++ src/cuda/level1/bfs/compare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in 1k 2k 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 16M
+for i in 1024 2048 4096 8192 65536 131072 261444 524288 1048576 2097152 4194304 8388608
     do
-        ./rbfs inputs/graph$i.txt > res_rodinia
-        ./bfs -i inputs/graph$i.txt --resultsfile r.txt --verbose -n 1 -o log > res_mirovia
+        ./rbfs inputs/bfs_$i > res_rodinia
+        ./bfs -i inputs/bfs_$i --resultsfile r.txt --verbose -n 1 -o log > res_mirovia
         (diff res_rodinia res_mirovia) >> test
         (diff result.txt r.txt) >> test_res
     done
