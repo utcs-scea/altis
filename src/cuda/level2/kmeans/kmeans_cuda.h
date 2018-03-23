952c05d4b1c031589998d08f76aa306fae126564
sarah wang <sarahgw@cs.utexas.edu>
2018-03-23 01:58:47 -0500

    almost done with kmeans, still need to do datagen
    Former-commit-id: aaf72b90cd2e13218d96a29a5b8e294b6787e80b
    Former-commit-id: 52fa6656df7b7b0db7cc188487837164ba46538e
    Former-commit-id: 76e4555b7f5692be5bb38797fe62c57e79cece0a

--- src/cuda/level2/kmeans/kmeans_cuda.h
+++ src/cuda/level2/kmeans/kmeans_cuda.h
@@ -1,6 +1,8 @@
 #ifndef _KMEANS_CUDA_H_
 #define _KMEANS_CUDA_H_
 
+#include "ResultDatabase.h"
+
 int	// delta -- had problems when return value was of float type
 kmeansCuda(float  **feature,				/* in: [npoints][nfeatures] */
            int      nfeatures,				/* number of attributes for each point */
@@ -9,8 +11,10 @@ kmeansCuda(float  **feature,				/* in: [npoints][nfeatures] */
            int     *membership,				/* which cluster the point belongs to */
 		   float  **clusters,				/* coordinates of cluster centers */
 		   int     *new_centers_len,		/* number of elements in each cluster */
-           float  **new_centers				/* sum of elements in each cluster */
-		   );
+           float  **new_centers,			/* sum of elements in each cluster */
+           double &transferTime,
+           double &kernelTime,
+		   ResultDatabase &resultDB);
 void allocateMemory(int npoints, int nfeatures, int nclusters, float **features);
 void deallocateMemory();
 
