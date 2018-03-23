a82f62ec4753051c6ca78d3377867de8d6b386f4
Christopher Denny <cdenny@zemaitis.csres.utexas.edu>
2018-03-22 23:06:00 -0500

    done with srad

--- src/cuda/level2/srad/srad.cu
+++ src/cuda/level2/srad/srad.cu
@@ -5,43 +5,334 @@
 #include <math.h>
 #include "srad.h"
 
+#include "cudacommon.h"
+#include "ResultDatabase.h"
+#include "OptionParser.h"
+
 // includes, project
 #include <cuda.h>
 
 // includes, kernels
-#include "srad_kernel.cu"
+//#include "srad_kernel.cu"
+
+float kernelTime = 0.0f;
+float transferTime = 0.0f;
+cudaEvent_t start, stop;
+float elapsed;
 
 void random_matrix(float *I, int rows, int cols);
 void runTest( int argc, char** argv);
-void usage(int argc, char **argv)
+void srad(ResultDatabase &resultDB, int imageSize, int speckleSize, int iters);
+
+__global__ void
+srad_cuda_1(
+		  float *E_C, 
+		  float *W_C, 
+		  float *N_C, 
+		  float *S_C,
+		  float * J_cuda, 
+		  float * C_cuda, 
+		  int cols, 
+		  int rows, 
+		  float q0sqr
+) 
 {
-	fprintf(stderr, "Usage: %s <rows> <cols> <y1> <y2> <x1> <x2> <lamda> <no. of iter>\n", argv[0]);
-	fprintf(stderr, "\t<rows>   - number of rows\n");
-	fprintf(stderr, "\t<cols>    - number of cols\n");
-	fprintf(stderr, "\t<y1> 	 - y1 value of the speckle\n");
-	fprintf(stderr, "\t<y2>      - y2 value of the speckle\n");
-	fprintf(stderr, "\t<x1>       - x1 value of the speckle\n");
-	fprintf(stderr, "\t<x2>       - x2 value of the speckle\n");
-	fprintf(stderr, "\t<lamda>   - lambda (0,1)\n");
-	fprintf(stderr, "\t<no. of iter>   - number of iterations\n");
-	
-	exit(1);
+
+  //block id
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+
+  //thread id
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  
+  //indices
+  int index   = cols * BLOCK_SIZE * by + BLOCK_SIZE * bx + cols * ty + tx;
+  int index_n = cols * BLOCK_SIZE * by + BLOCK_SIZE * bx + tx - cols;
+  int index_s = cols * BLOCK_SIZE * by + BLOCK_SIZE * bx + cols * BLOCK_SIZE + tx;
+  int index_w = cols * BLOCK_SIZE * by + BLOCK_SIZE * bx + cols * ty - 1;
+  int index_e = cols * BLOCK_SIZE * by + BLOCK_SIZE * bx + cols * ty + BLOCK_SIZE;
+
+  if(index_n >= rows * cols ||
+     index_s >= rows * cols ||
+     index_e >= rows * cols ||
+     index_w >= rows * cols ||
+     index_n < 0 ||
+     index_s < 0||
+     index_e < 0 ||
+     index_w < 0) {
+      return;
+  }
+
+  float n, w, e, s, jc, g2, l, num, den, qsqr, c;
+
+  //shared memory allocation
+  __shared__ float temp[BLOCK_SIZE][BLOCK_SIZE];
+  __shared__ float temp_result[BLOCK_SIZE][BLOCK_SIZE];
+
+  __shared__ float north[BLOCK_SIZE][BLOCK_SIZE];
+  __shared__ float south[BLOCK_SIZE][BLOCK_SIZE];
+  __shared__ float  east[BLOCK_SIZE][BLOCK_SIZE];
+  __shared__ float  west[BLOCK_SIZE][BLOCK_SIZE];
+
+  //load data to shared memory
+  north[ty][tx] = J_cuda[index_n]; 
+  south[ty][tx] = J_cuda[index_s];
+  if ( by == 0 ){
+  north[ty][tx] = J_cuda[BLOCK_SIZE * bx + tx]; 
+  }
+  else if ( by == gridDim.y - 1 ){
+  south[ty][tx] = J_cuda[cols * BLOCK_SIZE * (gridDim.y - 1) + BLOCK_SIZE * bx + cols * ( BLOCK_SIZE - 1 ) + tx];
+  }
+   __syncthreads();
+ 
+  west[ty][tx] = J_cuda[index_w];
+  east[ty][tx] = J_cuda[index_e];
+
+  if ( bx == 0 ){
+  west[ty][tx] = J_cuda[cols * BLOCK_SIZE * by + cols * ty]; 
+  }
+  else if ( bx == gridDim.x - 1 ){
+  east[ty][tx] = J_cuda[cols * BLOCK_SIZE * by + BLOCK_SIZE * ( gridDim.x - 1) + cols * ty + BLOCK_SIZE-1];
+  }
+ 
+  __syncthreads();
+  
+ 
+
+  temp[ty][tx]      = J_cuda[index];
+
+  __syncthreads();
+
+   jc = temp[ty][tx];
+
+   if ( ty == 0 && tx == 0 ){ //nw
+	n  = north[ty][tx] - jc;
+    s  = temp[ty+1][tx] - jc;
+    w  = west[ty][tx]  - jc; 
+    e  = temp[ty][tx+1] - jc;
+   }	    
+   else if ( ty == 0 && tx == BLOCK_SIZE-1 ){ //ne
+	n  = north[ty][tx] - jc;
+    s  = temp[ty+1][tx] - jc;
+    w  = temp[ty][tx-1] - jc; 
+    e  = east[ty][tx] - jc;
+   }
+   else if ( ty == BLOCK_SIZE -1 && tx == BLOCK_SIZE - 1){ //se
+	n  = temp[ty-1][tx] - jc;
+    s  = south[ty][tx] - jc;
+    w  = temp[ty][tx-1] - jc; 
+    e  = east[ty][tx]  - jc;
+   }
+   else if ( ty == BLOCK_SIZE -1 && tx == 0 ){//sw
+	n  = temp[ty-1][tx] - jc;
+    s  = south[ty][tx] - jc;
+    w  = west[ty][tx]  - jc; 
+    e  = temp[ty][tx+1] - jc;
+   }
+
+   else if ( ty == 0 ){ //n
+	n  = north[ty][tx] - jc;
+    s  = temp[ty+1][tx] - jc;
+    w  = temp[ty][tx-1] - jc; 
+    e  = temp[ty][tx+1] - jc;
+   }
+   else if ( tx == BLOCK_SIZE -1 ){ //e
+	n  = temp[ty-1][tx] - jc;
+    s  = temp[ty+1][tx] - jc;
+    w  = temp[ty][tx-1] - jc; 
+    e  = east[ty][tx] - jc;
+   }
+   else if ( ty == BLOCK_SIZE -1){ //s
+	n  = temp[ty-1][tx] - jc;
+    s  = south[ty][tx] - jc;
+    w  = temp[ty][tx-1] - jc; 
+    e  = temp[ty][tx+1] - jc;
+   }
+   else if ( tx == 0 ){ //w
+	n  = temp[ty-1][tx] - jc;
+    s  = temp[ty+1][tx] - jc;
+    w  = west[ty][tx] - jc; 
+    e  = temp[ty][tx+1] - jc;
+   }
+   else{  //the data elements which are not on the borders 
+	n  = temp[ty-1][tx] - jc;
+    s  = temp[ty+1][tx] - jc;
+    w  = temp[ty][tx-1] - jc; 
+    e  = temp[ty][tx+1] - jc;
+   }
+
+
+    g2 = ( n * n + s * s + w * w + e * e ) / (jc * jc);
+
+    l = ( n + s + w + e ) / jc;
+
+	num  = (0.5*g2) - ((1.0/16.0)*(l*l)) ;
+	den  = 1 + (.25*l);
+	qsqr = num/(den*den);
+
+	// diffusion coefficent (equ 33)
+	den = (qsqr-q0sqr) / (q0sqr * (1+q0sqr)) ;
+	c = 1.0 / (1.0+den) ;
+
+    // saturate diffusion coefficent
+	if (c < 0){temp_result[ty][tx] = 0;}
+	else if (c > 1) {temp_result[ty][tx] = 1;}
+	else {temp_result[ty][tx] = c;}
+
+    __syncthreads();
+
+    C_cuda[index] = temp_result[ty][tx];
+	E_C[index] = e;
+	W_C[index] = w;
+	S_C[index] = s;
+	N_C[index] = n;
+
 }
-////////////////////////////////////////////////////////////////////////////////
-// Program main
-////////////////////////////////////////////////////////////////////////////////
-int
-main( int argc, char** argv) 
+
+__global__ void
+srad_cuda_2(
+		  float *E_C, 
+		  float *W_C, 
+		  float *N_C, 
+		  float *S_C,	
+		  float * J_cuda, 
+		  float * C_cuda, 
+		  int cols, 
+		  int rows, 
+		  float lambda,
+		  float q0sqr
+) 
+{
+	//block id
+	int bx = blockIdx.x;
+    int by = blockIdx.y;
+
+	//thread id
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+	//indices
+    int index   = cols * BLOCK_SIZE * by + BLOCK_SIZE * bx + cols * ty + tx;
+	int index_s = cols * BLOCK_SIZE * by + BLOCK_SIZE * bx + cols * BLOCK_SIZE + tx;
+    int index_e = cols * BLOCK_SIZE * by + BLOCK_SIZE * bx + cols * ty + BLOCK_SIZE;
+	float cc, cn, cs, ce, cw, d_sum;
+
+	//shared memory allocation
+	__shared__ float south_c[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float  east_c[BLOCK_SIZE][BLOCK_SIZE];
+
+    __shared__ float c_cuda_temp[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float c_cuda_result[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float temp[BLOCK_SIZE][BLOCK_SIZE];
+
+    //load data to shared memory
+	temp[ty][tx]      = J_cuda[index];
+
+    __syncthreads();
+	 
+	south_c[ty][tx] = C_cuda[index_s];
+
+	if ( by == gridDim.y - 1 ){
+	south_c[ty][tx] = C_cuda[cols * BLOCK_SIZE * (gridDim.y - 1) + BLOCK_SIZE * bx + cols * ( BLOCK_SIZE - 1 ) + tx];
+	}
+	__syncthreads();
+	 
+	 
+	east_c[ty][tx] = C_cuda[index_e];
+	
+	if ( bx == gridDim.x - 1 ){
+	east_c[ty][tx] = C_cuda[cols * BLOCK_SIZE * by + BLOCK_SIZE * ( gridDim.x - 1) + cols * ty + BLOCK_SIZE-1];
+	}
+	 
+    __syncthreads();
+  
+    c_cuda_temp[ty][tx]      = C_cuda[index];
+
+    __syncthreads();
+
+	cc = c_cuda_temp[ty][tx];
+
+   if ( ty == BLOCK_SIZE -1 && tx == BLOCK_SIZE - 1){ //se
+	cn  = cc;
+    cs  = south_c[ty][tx];
+    cw  = cc; 
+    ce  = east_c[ty][tx];
+   } 
+   else if ( tx == BLOCK_SIZE -1 ){ //e
+	cn  = cc;
+    cs  = c_cuda_temp[ty+1][tx];
+    cw  = cc; 
+    ce  = east_c[ty][tx];
+   }
+   else if ( ty == BLOCK_SIZE -1){ //s
+	cn  = cc;
+    cs  = south_c[ty][tx];
+    cw  = cc; 
+    ce  = c_cuda_temp[ty][tx+1];
+   }
+   else{ //the data elements which are not on the borders 
+	cn  = cc;
+    cs  = c_cuda_temp[ty+1][tx];
+    cw  = cc; 
+    ce  = c_cuda_temp[ty][tx+1];
+   }
+
+   // divergence (equ 58)
+   d_sum = cn * N_C[index] + cs * S_C[index] + cw * W_C[index] + ce * E_C[index];
+
+   // image update (equ 61)
+   c_cuda_result[ty][tx] = temp[ty][tx] + 0.25 * lambda * d_sum;
+
+   __syncthreads();
+              
+   J_cuda[index] = c_cuda_result[ty][tx];
+    
+}
+
+void addBenchmarkSpecOptions(OptionParser &op) {
+    op.addOption("imageSize", OPT_INT, "0", "image height and width");
+    op.addOption("speckleSize", OPT_INT, "0", "speckle height and width");
+    op.addOption("iterations", OPT_INT, "0", "iterations of algorithm");
+}
+
+void RunBenchmark(ResultDatabase &resultDB, OptionParser &op) 
 {
   printf("WG size of kernel = %d X %d\n", BLOCK_SIZE, BLOCK_SIZE);
-    runTest( argc, argv);
+  int imageSize = op.getOptionInt("imageSize");
+  int speckleSize = op.getOptionInt("speckleSize");
+  int iters = op.getOptionInt("iterations");
+  if(imageSize == 0 || speckleSize == 0 || iters == 0) {
+      int imageSizes[4] = {128, 512, 4096, 8192};
+      int iterSizes[4] = {5, 50, 100, 200};
+      imageSize = imageSizes[op.getOptionInt("size") - 1];
+      speckleSize = imageSize / 2;
+      iters = iterSizes[op.getOptionInt("size") - 1];
+  }
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
 
-    return EXIT_SUCCESS;
+  printf("Image Size: %d by %d\n", imageSize, imageSize);
+  printf("Speckle size: %d by %d\n", speckleSize, speckleSize);
+  printf("Num Iterations: %d\n", iters);
+
+  int passes = op.getOptionInt("passes");
+  for(int i = 0; i < passes; i++) {
+      kernelTime = 0.0f;
+      transferTime = 0.0f;
+      printf("Pass %d:\n", i);
+      srad(resultDB, imageSize, speckleSize, iters);
+      printf("Done.\n");
+      char atts[1024];
+      sprintf(atts, "img:%d,speckle:%d,iter:%d", imageSize, speckleSize, iters);
+      resultDB.AddResult("srad_kernel_time", atts, "sec", kernelTime);
+      resultDB.AddResult("srad_transfer_time", atts, "sec", transferTime);
+      resultDB.AddResult("srad_parity", atts, "N", transferTime / kernelTime);
+  }
 }
 
 
-void
-runTest( int argc, char** argv) 
+void srad(ResultDatabase &resultDB, int imageSize, int speckleSize, int iters) 
 {
     int rows, cols, size_I, size_R, niter = 10, iter;
     float *I, *J, lambda, q0sqr, sum, sum2, tmp, meanROI,varROI ;
@@ -66,26 +357,18 @@ runTest( int argc, char** argv)
     
 	
  
-	if (argc == 9)
-	{
-		rows = atoi(argv[1]);  //number of rows in the domain
-		cols = atoi(argv[2]);  //number of cols in the domain
-		if ((rows%16!=0) || (cols%16!=0)){
-		fprintf(stderr, "rows and cols must be multiples of 16\n");
-		exit(1);
-		}
-		r1   = atoi(argv[3]);  //y1 position of the speckle
-		r2   = atoi(argv[4]);  //y2 position of the speckle
-		c1   = atoi(argv[5]);  //x1 position of the speckle
-		c2   = atoi(argv[6]);  //x2 position of the speckle
-		lambda = atof(argv[7]); //Lambda value
-		niter = atoi(argv[8]); //number of iterations
-		
-	}
-    else{
-	usage(argc, argv);
+    rows = imageSize;  //number of rows in the domain
+    cols = imageSize;  //number of cols in the domain
+    if ((rows%16!=0) || (cols%16!=0)){
+        fprintf(stderr, "rows and cols must be multiples of 16\n");
+        exit(1);
     }
-
+    r1   = 0;  //y1 position of the speckle
+    r2   = speckleSize;  //y2 position of the speckle
+    c1   = 0;  //x1 position of the speckle
+    c2   = speckleSize;  //x2 position of the speckle
+    lambda = 0.5; //Lambda value
+    niter = iters; //number of iterations
 
 
 	size_I = cols * rows;
@@ -128,24 +411,22 @@ runTest( int argc, char** argv)
 #ifdef GPU
 
 	//Allocate device memory
-    cudaMalloc((void**)& J_cuda, sizeof(float)* size_I);
-    cudaMalloc((void**)& C_cuda, sizeof(float)* size_I);
-	cudaMalloc((void**)& E_C, sizeof(float)* size_I);
-	cudaMalloc((void**)& W_C, sizeof(float)* size_I);
-	cudaMalloc((void**)& S_C, sizeof(float)* size_I);
-	cudaMalloc((void**)& N_C, sizeof(float)* size_I);
+    CUDA_SAFE_CALL(cudaMalloc((void**)& J_cuda, sizeof(float)* size_I));
+    CUDA_SAFE_CALL(cudaMalloc((void**)& C_cuda, sizeof(float)* size_I));
+	CUDA_SAFE_CALL(cudaMalloc((void**)& E_C, sizeof(float)* size_I));
+	CUDA_SAFE_CALL(cudaMalloc((void**)& W_C, sizeof(float)* size_I));
+	CUDA_SAFE_CALL(cudaMalloc((void**)& S_C, sizeof(float)* size_I));
+	CUDA_SAFE_CALL(cudaMalloc((void**)& N_C, sizeof(float)* size_I));
 
 	
 #endif 
 
-	printf("Randomizing the input matrix\n");
 	//Generate a random matrix
 	random_matrix(I, rows, cols);
 
     for (int k = 0;  k < size_I; k++ ) {
      	J[k] = (float)exp(I[k]) ;
     }
-	printf("Start the SRAD main loop\n");
  for (iter=0; iter< niter; iter++){     
 		sum=0; sum2=0;
         for (int i=r1; i<=r2; i++) {
@@ -225,14 +506,37 @@ runTest( int argc, char** argv)
     
 
 	//Copy data from main memory to device memory
-	cudaMemcpy(J_cuda, J, sizeof(float) * size_I, cudaMemcpyHostToDevice);
+    cudaEventRecord(start, 0);
+	CUDA_SAFE_CALL(cudaMemcpy(J_cuda, J, sizeof(float) * size_I, cudaMemcpyHostToDevice));
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsed, start, stop);
+    transferTime += elapsed * 1.e-3;
 
 	//Run kernels
+    cudaEventRecord(start, 0);
 	srad_cuda_1<<<dimGrid, dimBlock>>>(E_C, W_C, N_C, S_C, J_cuda, C_cuda, cols, rows, q0sqr); 
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsed, start, stop);
+    kernelTime += elapsed * 1.e-3;
+    CHECK_CUDA_ERROR();
+
+    cudaEventRecord(start, 0);
 	srad_cuda_2<<<dimGrid, dimBlock>>>(E_C, W_C, N_C, S_C, J_cuda, C_cuda, cols, rows, lambda, q0sqr); 
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsed, start, stop);
+    kernelTime += elapsed * 1.e-3;
+    CHECK_CUDA_ERROR();
 
 	//Copy data from device memory to main memory
-    cudaMemcpy(J, J_cuda, sizeof(float) * size_I, cudaMemcpyDeviceToHost);
+    cudaEventRecord(start, 0);
+    CUDA_SAFE_CALL(cudaMemcpy(J, J_cuda, sizeof(float) * size_I, cudaMemcpyDeviceToHost));
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsed, start, stop);
+    transferTime += elapsed * 1.e-3;
 
 #endif   
 }
@@ -250,7 +554,6 @@ runTest( int argc, char** argv)
    }
 #endif 
 
-	printf("Computation Done\n");
 
 	free(I);
 	free(J);
@@ -259,12 +562,12 @@ runTest( int argc, char** argv)
     free(dN); free(dS); free(dW); free(dE);
 #endif
 #ifdef GPU
-    cudaFree(C_cuda);
-	cudaFree(J_cuda);
-	cudaFree(E_C);
-	cudaFree(W_C);
-	cudaFree(N_C);
-	cudaFree(S_C);
+    CUDA_SAFE_CALL(cudaFree(C_cuda));
+	CUDA_SAFE_CALL(cudaFree(J_cuda));
+	CUDA_SAFE_CALL(cudaFree(E_C));
+	CUDA_SAFE_CALL(cudaFree(W_C));
+	CUDA_SAFE_CALL(cudaFree(N_C));
+	CUDA_SAFE_CALL(cudaFree(S_C));
 #endif 
 	free(c);
   
@@ -282,4 +585,3 @@ void random_matrix(float *I, int rows, int cols){
 	}
 
 }
-
