69373109a8b806c0129c75c82c93644460393c66
sarah wang <sarahgw@cs.utexas.edu>
2018-04-08 16:27:45 -0500

    fixed sort
    Former-commit-id: b769327be8e9dc86648320983d7d918f3ef39ac1
    Former-commit-id: 31844066cde6573164ec4e5bb00c13dd0c35d92d
    Former-commit-id: ddf88be6a231075de05936c59170645f583977a8

--- src/cuda/level1/sort/Sort.h
+++ src/cuda/level1/sort/Sort.h
@@ -7,15 +7,18 @@ static const int SORT_BLOCK_SIZE = 128;
 static const int SCAN_BLOCK_SIZE = 256;
 static const int SORT_BITS = 32;
 
-void radixSortStep(uint nbits, uint startbit, uint4 *keys, uint4 *values,
-                   uint4 *tempKeys, uint4 *tempValues, uint *counters,
-                   uint *countersSum, uint *blockOffsets, uint **scanBlockSums,
-                   uint numElements);
+void
+radixSortStep(uint nbits, uint startbit, uint4* keys, uint4* values,
+        uint4* tempKeys, uint4* tempValues, uint* counters,
+        uint* countersSum, uint* blockOffsets, uint** scanBlockSums,
+        uint numElements);
 
-void scanArrayRecursive(uint *outArray, uint *inArray, int numElements,
-                        int level, uint **blockSums);
+void
+scanArrayRecursive(uint* outArray, uint* inArray, int numElements, int level,
+        uint** blockSums);
 
-bool verifySort(uint *keys, uint *vals, const size_t size, bool verbose);
+bool
+verifySort(uint *keys, uint* vals, const size_t size, bool verbose);
 
 #ifdef __DEVICE_EMULATION__
 #define __SYNC __syncthreads();
