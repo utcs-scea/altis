755bb4c4cf28d3ca1c073071d9177edf1ae5c007
BDHU <bodunhu@utexas.edu>
2019-03-08 18:50:55 -0600

    added cuda graph in configure, fdtd ni progress, not working yet

--- src/cuda/level2/fdtd2d/fdtd.h
+++ src/cuda/level2/fdtd2d/fdtd.h
@@ -21,13 +21,13 @@
 #define NX 2048
 #define NY 2048
 
+// TODO: Maybe utilize half precision?
 #ifndef DATA_TYPE
 #define DATA_TYPE float
 #endif
 
-__global__ void init_arrays(DATA_TYPE *_fict_, DATA_TYPE *ex,
-                            DATA_TYPE *ey, DATA_TYPE *hz);
-void cpu_run_fdtd(DATA_TYPE *_fict_, DATA_TYPE *ex, DATA_TYPE *ey, DATA_TYPE *hz);
+void init_arrays(DATA_TYPE *_fict_, DATA_TYPE *ex, DATA_TYPE *ey, DATA_TYPE *hz);
+void run_fdtd_cuda(DATA_TYPE *_fict_, DATA_TYPE *ex, DATA_TYPE *ey, DATA_TYPE *hz);
 void compare_results(DATA_TYPE *hz1, DATA_TYPE *hz2);
 
 
