57017c84b4ae6420d7b0bfb7255fd2f776f0590f
BDHU <bodunhu@utexas.edu>
2019-03-29 16:57:57 -0500

    make gups run

--- driver.py
+++ driver.py
@@ -3,8 +3,8 @@ import subprocess
 import sys
 from optparse import OptionParser
 
-suite = ['busspeeddownload', 'busspeedreadback', 'devicememory', 'maxflops', 'bfs', 'gemm', 'pathfinder', 'sort', 'cfd', 'dwt2d', 'fdtd2d', 'kmeans', 'lavamd', 'mandelbrot', 'nw', 'particlefilter', 'srad', 'where']
-suite_map = {'busspeeddownload':0, 'busspeedreadback':0, 'devicememory':0, 'maxflops':0, 'bfs':1, 'gemm':1, 'pathfinder':1, 'sort':1, 'cfd':2, 'dwt2d':2, 'fdtd2d':2, 'kmeans':2, 'lavamd':2, 'mandelbrot':2, 'nw':2, 'particlefilter':2, 'srad':2, 'where':2}
+suite = ['busspeeddownload', 'busspeedreadback', 'devicememory', 'maxflops', 'bfs', 'gemm', 'pathfinder', 'sort', 'cfd', 'dwt2d', 'fdtd2d', 'gups', 'kmeans', 'lavamd', 'mandelbrot', 'nw', 'particlefilter', 'srad', 'where']
+suite_map = {'busspeeddownload':0, 'busspeedreadback':0, 'devicememory':0, 'maxflops':0, 'bfs':1, 'gemm':1, 'pathfinder':1, 'sort':1, 'cfd':2, 'dwt2d':2, 'fdtd2d':2, 'gups':2, 'kmeans':2, 'lavamd':2, 'mandelbrot':2, 'nw':2, 'particlefilter':2, 'srad':2, 'where':2}
 
 def run_benchmark(options, b):
     print('*****')
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     parser.add_option('-s', '--size', default=0, help='problem size')
     parser.add_option('-b', '--benchmark', default='all', help='comma-separated list of benchmarks to run, or \'all\' to run entire suite, defaults to \'all\'')
     parser.add_option('-v', '--verbose', action='store_true', help='enable verbose output')
+    parser.add_option('\0', '--shifts', default=20, help='size of update table')   # Only for GUPS
     
     # Parse options
     (options, args) = parser.parse_args()
