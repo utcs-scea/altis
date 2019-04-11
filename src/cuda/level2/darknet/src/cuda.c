205bbe194098a9c99748e35da6f9913e12bd9416
BDHU <bodunhu@utexas.edu>
2019-04-11 02:02:26 -0500

    make code slightly easier to read

--- src/cuda/level2/darknet/src/cuda.c
+++ src/cuda/level2/darknet/src/cuda.c
@@ -52,7 +52,7 @@ dim3 cuda_gridsize(size_t n){
     size_t k = (n-1) / BLOCK + 1;
     size_t x = k;
     size_t y = 1;
-    if(x > 65535){
+    if(x > GRID_MAX_PER_DIM){
         x = ceil(sqrt(k));
         y = (n-1)/(x*BLOCK) + 1;
     }
