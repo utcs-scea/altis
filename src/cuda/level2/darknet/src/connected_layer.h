eef78b13e0503a37fece3463b9466f2eaba7bc4c
BDHU <bodunhu@utexas.edu>
2019-04-30 23:05:43 -0500

    Most disgusting Makefile but ok it works
    Former-commit-id: 26abab36bd724d4c66b2941a56a11e9e1f8b7c9f
    Former-commit-id: f7b715726f3f81b192c87a7a624b5c6dcbf89d31
    Former-commit-id: 439a78132779174e6c2d6aa0cb89bbf8c9f0d66d

--- src/cuda/level2/darknet/src/connected_layer.h
+++ src/cuda/level2/darknet/src/connected_layer.h
@@ -5,14 +5,13 @@
 #include "layer.h"
 #include "network.h"
 
-void test_connected_layer_forward(void);
+//void test_connected_layer_forward(void);
 
 layer make_connected_layer(int batch, int inputs, int outputs, ACTIVATION activation, int batch_normalize, int adam);
 
 void forward_connected_layer(layer l, network net);
 void backward_connected_layer(layer l, network net);
 void update_connected_layer(layer l, update_args a);
-void test_connected_layer_forward(void);
 
 #ifdef GPU
 void forward_connected_layer_gpu(layer l, network net);
